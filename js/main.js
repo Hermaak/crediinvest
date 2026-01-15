ec8cf4e3cc9d3438491390734b2c3ff257edfa2e
sairessy <sairessy@gmail.com>
2026-01-15 12:38:59 +0200

    hamb-menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let menuHidden = true;
 
-document.querySelector(".btn-menu").addEventListener("click", () => {
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
   $(".menu").animate({ left: menuHidden ? 0 : "-80%" }, 500);
   menuHidden = !menuHidden;
 });

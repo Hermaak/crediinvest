10ec2588ffe8f9449d609198b9779727d28baa67
sairessy <sairessy@gmail.com>
2026-02-01 10:36:26 +0200

    pdf 2x

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const api_url = 'http://localhost:8000'
-// const api_url = 'https://api-master.onrender.com'
+// const api_url = 'http://localhost:8000'
+const api_url = 'https://api-master.onrender.com'
 
 let menuHidden = true;
 

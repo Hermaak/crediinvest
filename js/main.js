6766b67c87543511d7693097ed7960263057932d
sairessy <sairessy@gmail.com>
2026-02-02 10:05:17 +0200

    styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-// const api_url = 'http://localhost:8000'
-const api_url = 'https://api-master.onrender.com'
+const api_url = "http://localhost:8000";
+// const api_url = "https://api-master.onrender.com";
 
 let menuHidden = true;
 
@@ -9,3 +9,18 @@ hamburger.addEventListener("click", function () {
   $(".menu").animate({ left: menuHidden ? 0 : "-80%" }, 500);
   menuHidden = !menuHidden;
 });
+
+awakeAPI();
+
+setInterval(async () => {
+  awakeAPI();
+}, 30000);
+
+async function awakeAPI() {
+  try {
+    const res = await fetch(api_url);
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
